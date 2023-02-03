51219ca1d3e37433bff3dd399ac519f457af5b45
Thomas Vaudescal <vaudescal.t@gmail.com>
2023-02-03 10:12:55 -0500

    [ADD] | formatting + documentation

--- utils.py
+++ utils.py
@@ -6,55 +6,94 @@ from scipy.stats import norm
 import math
 import matplotlib.pyplot as plt
 
+
 def get_info():
-    K  = np.array([80, 90, 97.5, 102.5, 110, 120])
+    K = np.array([80, 90, 97.5, 102.5, 110, 120])
     Put = np.array([0.1900, 0.6907, 1.6529, 3.3409, 9.8399, 19.5805])
-    return pd.DataFrame(np.vstack([K,Put]).T, columns=['Strike','Put'])
+    return pd.DataFrame(np.vstack([K, Put]).T, columns=["Strike", "Put"])
+
 
 def compute_put_option(sigma: float, S0: float, K: float, T: float, r: float) -> float:
-    d1 = (np.log(S0/K) + (r + sigma**2/2)*T)/(sigma*np.sqrt(T))
-    d2 = d1 - sigma*np.sqrt(T)
-    return K*np.exp(-r*T)*norm.cdf(-d2) - S0*norm.cdf(-d1)
+    """Compute the price of a European put option.
+    :param sigma: volatility
+    :param S0: initial stock price
+    :param K: strike price
+    :param T: maturity
+    :param r: risk-free interest rate
+    :return: European put option price"""
 
-def compute_implied_volatility_by_bisection(S0: float,
-                                            K: float,
-                                            T: float,
-                                            r: float,
-                                            Put: float,
-                                            tol: float = 1e-6,
-                                            max_iter: int = 1000) -> float:
+    d1 = (np.log(S0 / K) + (r + sigma**2 / 2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    return K * np.exp(-r * T) * norm.cdf(-d2) - S0 * norm.cdf(-d1)
+
+
+def compute_implied_volatility_by_bisection(
+    S0: float,
+    K: float,
+    T: float,
+    r: float,
+    Put: float,
+    tol: float = 1e-6,
+    max_iter: int = 1000,
+) -> float:
+    """Compute the implied volatility by bisection method.
+    :param S0: initial stock price
+    :param K: strike price
+    :param T: maturity
+    :param r: risk-free interest rate
+    :param Put: put price
+    :param tol: maximum precision tolerance
+    :param max_iter: maximum number of iterations
+    :return: implied volatility"""
 
     # Initialisation
     sigma_min = 0.0
     sigma_max = 1.0
-    sigma = (sigma_min + sigma_max)/2
-    Put_calcul = compute_put_option(sigma=sigma, S0=S0, K=K, T=T, r=r)
+    sigma = (sigma_min + sigma_max) / 2
+    put_estimation = compute_put_option(sigma=sigma, S0=S0, K=K, T=T, r=r)
     i = 0
 
     # Boucle
-    while abs(Put_calcul - Put) > tol and i < max_iter:
-        if Put_calcul > Put:
+    while abs(put_estimation - Put) > tol and i < max_iter:
+        if put_estimation > Put:
             sigma_max = sigma
         else:
             sigma_min = sigma
-        sigma = (sigma_min + sigma_max)/2
-        Put_calcul = compute_put_option(sigma=sigma, S0=S0, K=K, T=T, r=r)
+        sigma = (sigma_min + sigma_max) / 2
+        put_estimation = compute_put_option(sigma=sigma, S0=S0, K=K, T=T, r=r)
         i += 1
 
     return sigma
 
-def compute_implicit_vol_array(S0: float, K: ndarray, T: float, r: float, Put: ndarray) -> ndarray:
-    return np.array([compute_implied_volatility_by_bisection(S0=S0,
-                                                             K=K[i],
-                                                             T=T,
-                                                             r=r,
-                                                             Put=Put[i])
-                     for i in range(len(K))])
+
+def compute_implicit_vol_array(
+    S0: float, K: ndarray, T: float, r: float, Put: ndarray
+) -> ndarray:
+    """Compute the implied volatility for a vector of strike prices and put prices.
+    :param S0: initial stock price
+    :param K: vector of strike prices
+    :param T: maturity
+    :param r: risk-free interest rate
+    :param Put: vector of put prices
+    :return: vector of implied volatilities"""
+    return np.array(
+        [
+            compute_implied_volatility_by_bisection(S0=S0, K=K[i], T=T, r=r, Put=Put[i])
+            for i in range(len(K))
+        ]
+    )
 
 
-def CRR_Tree(S: float, K: float, T: float, r: float, sigma: float, N: int)\
-        -> float:
+def CRR_Tree(S: float, K: float, T: float, r: float, sigma: float, N: int) -> float:
 
+    """Cox-Ross-Rubinstein binomial tree for European put option.
+    :param S: initial stock price
+    :param K: strike price
+    :param T: maturity
+    :param r: risk-free interest rate
+    :param sigma: volatility
+    :param N: number of time steps
+    :return: European put option price"""
 
     u = math.exp(sigma * math.sqrt(T / N))
     d = math.exp(-sigma * math.sqrt(T / N))
@@ -64,10 +103,10 @@ def CRR_Tree(S: float, K: float, T: float, r: float, sigma: float, N: int)\
 
     discount = math.exp(-r * T / N)
 
-    S_t = np.zeros(N+1)
-    puts = np.zeros(N+1)
+    S_t = np.zeros(N + 1)
+    puts = np.zeros(N + 1)
 
-    S_t[0] = S * d ** N
+    S_t[0] = S * d**N
 
     for j in range(1, N + 1):
         S_t[j] = S_t[j - 1] * (u / d)
@@ -81,13 +120,16 @@ def CRR_Tree(S: float, K: float, T: float, r: float, sigma: float, N: int)\
 
     return puts[0]
 
+
 # Convert matrix to dataframe
-def matrix_to_dataframe(matrix: ndarray, columns: Iterable[str]) -> pd.DataFrame:
+def matrix_to_dataframe(matrix: ndarray, columns: list[str]) -> pd.DataFrame:
+    """Convert a matrix to a dataframe.
+    :param matrix: matrix
+    :param columns: list of columns names
+    :return: dataframe"""
+
     return pd.DataFrame(matrix, columns=columns)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass
-
-
-
